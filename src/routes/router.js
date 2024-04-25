14456bb45d02555d688f538b71ccf3769ca1fe7b
EdithLizcano <126712189+EdithLizcano@users.noreply.github.com>
2024-04-25 09:45:34 -0600

    funciones

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const { verify } = require("../controllers/Verify.js");
+const { createIncidencias, deleteIncidencias, getIncidencias, editarIncidencias } = require("../controllers/Incidencias.js");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -9,5 +10,20 @@ const upload = multer({ storage: storage });
 router.get("/verify", (req, res) => {
   verify(req, res);
 });
+
+//incidencias
+router.post("/Incidencias",(req,res)=>{
+  createIncidencias(req, res)
+})
+router.delete("/Incidencias",(req,res)=>{
+  deleteIncidencias(req, res)
+})
+router.get("/Incidencias",(req,res)=>{
+  getIncidencias(req, res)
+})
+router.put("/Incidencias",(req,res)=>{
+  editarIncidencias(req, res)
+})
+
 
 module.exports = router;
