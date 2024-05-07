6924f9faa8ed2d0aa1904a9f0a7ccf2dc88ebfb7
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2024-05-07 12:24:54 -0600

    ajustes

--- src/controllers/Incidencias.js
+++ src/controllers/Incidencias.js
@@ -1,138 +1,14 @@
 const utils = require("../utils/responseBuilder.js");
 
-
 module.exports = {
-
-createIncidencias: async (req, res) =>{
-
+  createIncidencias: async (req, res) => {
     try {
-        const{Estatus,TextoInc,NombreRegistra,EmailRegistra,IdUsuario}= req.body;
-        const result = await utils.executeQuery("CALL sp_CrearIncidencia(?,?,?,?,?)", [
-            Estatus,TextoInc,NombreRegistra,EmailRegistra,IdUsuario,
-        ]);
-  
-        if (result.length > 2) {
-          const data = result;
-          const responseData = utils.buildResponse(
-            data[0],
-            true,
-            data[1][0].Respuesta,
-            data[1][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        } else {
-          const responseData = utils.buildResponse(
-            [],
-            true,
-            result[0][0].Respuesta,
-            result[0][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        }
-      } catch (error) {
-        const responseData = utils.buildResponse(null, false, 500, error.message);
-        res.status(500).json(responseData);
-      }
-    
-},
-deleteIncidencias: async (req, res) =>{
-
-    try {
-        const{IdIncidencia,IdUsuario}= req.body;
-
-        const result = await utils.executeQuery("CALL sp_EliminarIncidencia(?,?)", [
-            IdIncidencia,IdUsuario,
-        ]);
-  
-        if (result.length > 2) {
-          const data = result;
-          const responseData = utils.buildResponse(
-            data[0],
-            true,
-            data[1][0].Respuesta,
-            data[1][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        } else {
-          const responseData = utils.buildResponse(
-            [],
-            true,
-            result[0][0].Respuesta,
-            result[0][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        }
-      } catch (error) {
-        const responseData = utils.buildResponse(null, false, 500, error.message);
-        res.status(500).json(responseData);
-      }
-},
-getIncidencias: async (req, res) =>{
-
-    try {
-
-        const result = await utils.executeQuery("CALL sp_ListaIncidencias()", [ ]);
-  
-        if (result.length > 2) {
-          const data = result;
-          const responseData = utils.buildResponse(
-            data[0],
-            true,
-            data[1][0].Respuesta,
-            data[1][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        } else {
-          const responseData = utils.buildResponse(
-            [],
-            true,
-            result[0][0].Respuesta,
-            result[0][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        }
-      } catch (error) {
-        const responseData = utils.buildResponse(null, false, 500, error.message);
-        res.status(500).json(responseData);
-      }
-},
-editarIncidencias: async (req, res) =>{
-
-
-    try {
-        const{CHID,Estatus,TextoInc,NombreRegistra,EmailRegistra,CHUSER,Observaciones,AsignadoA,Prioridades}= req.body;
-
-        const result = await utils.executeQuery("CALL sp_EditarIncidencia(?,?,?,?,?,?,?,?,?)", [
-            CHID,Estatus,TextoInc,NombreRegistra,EmailRegistra,CHUSER,Observaciones,AsignadoA,Prioridades]);
-  
-        if (result.length > 2) {
-          const data = result;
-          const responseData = utils.buildResponse(
-            data[0],
-            true,
-            data[1][0].Respuesta,
-            data[1][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        } else {
-          const responseData = utils.buildResponse(
-            [],
-            true,
-            result[0][0].Respuesta,
-            result[0][0].Mensaje
-          );
-          res.status(200).json(responseData);
-        }
-      } catch (error) {
-        const responseData = utils.buildResponse(null, false, 500, error.message);
-        res.status(500).json(responseData);
-      }
-},
-getHistorial: async (req, res) =>{
-
-  try {
-      const IdIncidencia= req.query.IdIncidencia; 
-      const result = await utils.executeQuery("CALL sp_ListaHistorial(?)", [IdIncidencia ]);
+      const { Estatus, TextoInc, NombreRegistra, EmailRegistra, IdUsuario } =
+        req.body;
+      const result = await utils.executeQuery(
+        "CALL sp_CrearIncidencia(?,?,?,?,?)",
+        [Estatus, TextoInc, NombreRegistra, EmailRegistra, IdUsuario]
+      );
 
       if (result.length > 2) {
         const data = result;
@@ -156,5 +32,179 @@ getHistorial: async (req, res) =>{
       const responseData = utils.buildResponse(null, false, 500, error.message);
       res.status(500).json(responseData);
     }
-},
-}
+  },
+  deleteIncidencias: async (req, res) => {
+    try {
+      const { IdIncidencia, IdUsuario } = req.body;
+
+      const result = await utils.executeQuery(
+        "CALL sp_EliminarIncidencia(?,?)",
+        [IdIncidencia, IdUsuario]
+      );
+
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+
+  getIncidenciasbyuser: async (req, res) => {
+    try {
+      const CHID = req.query.CHID;
+      console.log(CHID);
+      const result = await utils.executeQuery(
+        "CALL sp_ListaIncidenciasbyUser(?)",
+        [CHID]
+      );
+
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+  getIncidencias: async (req, res) => {
+    try {
+      const result = await utils.executeQuery("CALL sp_ListaIncidencias()", []);
+
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+  editarIncidencias: async (req, res) => {
+    try {
+      const {
+        CHID,
+        Estatus,
+        TextoInc,
+        NombreRegistra,
+        EmailRegistra,
+        CHUSER,
+        Observaciones,
+        AsignadoA,
+        Prioridades,
+      } = req.body;
+
+      const result = await utils.executeQuery(
+        "CALL sp_EditarIncidencia(?,?,?,?,?,?,?,?,?)",
+        [
+          CHID,
+          Estatus,
+          TextoInc,
+          NombreRegistra,
+          EmailRegistra,
+          CHUSER,
+          Observaciones,
+          AsignadoA,
+          Prioridades,
+        ]
+      );
+
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+  getHistorial: async (req, res) => {
+    try {
+      const IdIncidencia = req.query.IdIncidencia;
+      const result = await utils.executeQuery("CALL sp_ListaHistorial(?)", [
+        IdIncidencia,
+      ]);
+
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+};
--- src/controllers/Select.js
+++ src/controllers/Select.js
@@ -1,93 +1,118 @@
 const utils = require("../utils/responseBuilder.js");
 
-
 module.exports = {
-    getEstado: async (req, res) =>{
+  getEstado: async (req, res) => {
+    try {
+      const result = await utils.executeQuery("CALL sp_ListaEstado()", []);
 
-        try {
-    
-            const result = await utils.executeQuery("CALL sp_ListaEstado()", [ ]);
-      
-            if (result.length > 2) {
-              const data = result;
-              const responseData = utils.buildResponse(
-                data[0],
-                true,
-                data[1][0].Respuesta,
-                data[1][0].Mensaje
-              );
-              res.status(200).json(responseData);
-            } else {
-              const responseData = utils.buildResponse(
-                [],
-                true,
-                result[0][0].Respuesta,
-                result[0][0].Mensaje
-              );
-              res.status(200).json(responseData);
-            }
-          } catch (error) {
-            const responseData = utils.buildResponse(null, false, 500, error.message);
-            res.status(500).json(responseData);
-          }
-    },
-    getUsuarioInci: async (req, res) =>{
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+  getUsuarioInci: async (req, res) => {
+    try {
+      const result = await utils.executeQuery(
+        "CALL sp_ListaUsuariosInci()",
+        []
+      );
 
-        try {
-    
-            const result = await utils.executeQuery("CALL sp_ListaUsuariosInci()", [ ]);
-      
-            if (result.length > 2) {
-              const data = result;
-              const responseData = utils.buildResponse(
-                data[0],
-                true,
-                data[1][0].Respuesta,
-                data[1][0].Mensaje
-              );
-              res.status(200).json(responseData);
-            } else {
-              const responseData = utils.buildResponse(
-                [],
-                true,
-                result[0][0].Respuesta,
-                result[0][0].Mensaje
-              );
-              res.status(200).json(responseData);
-            }
-          } catch (error) {
-            const responseData = utils.buildResponse(null, false, 500, error.message);
-            res.status(500).json(responseData);
-          }
-    },
-    getPrioridad: async (req, res) =>{
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+  getPrioridad: async (req, res) => {
+    try {
+      const result = await utils.executeQuery("CALL sp_ListaPrioridad()", []);
 
-        try {
-    
-            const result = await utils.executeQuery("CALL sp_ListaPrioridad()", [ ]);
-      
-            if (result.length > 2) {
-              const data = result;
-              const responseData = utils.buildResponse(
-                data[0],
-                true,
-                data[1][0].Respuesta,
-                data[1][0].Mensaje
-              );
-              res.status(200).json(responseData);
-            } else {
-              const responseData = utils.buildResponse(
-                [],
-                true,
-                result[0][0].Respuesta,
-                result[0][0].Mensaje
-              );
-              res.status(200).json(responseData);
-            }
-          } catch (error) {
-            const responseData = utils.buildResponse(null, false, 500, error.message);
-            res.status(500).json(responseData);
-          }
-    },
-
-}
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+  getEstadoNext: async (req, res) => {
+    try {
+      const TIPO = req.query.TIPO;
+      console.log(TIPO);
+      const result = await utils.executeQuery("CALL sp_ListaEstadoNext(?)", [
+        TIPO,
+      ]);
+      if (result.length > 2) {
+        const data = result;
+        const responseData = utils.buildResponse(
+          data[0],
+          true,
+          data[1][0].Respuesta,
+          data[1][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      } else {
+        const responseData = utils.buildResponse(
+          [],
+          true,
+          result[0][0].Respuesta,
+          result[0][0].Mensaje
+        );
+        res.status(200).json(responseData);
+      }
+    } catch (error) {
+      const responseData = utils.buildResponse(null, false, 500, error.message);
+      res.status(500).json(responseData);
+    }
+  },
+};
